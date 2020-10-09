2568e57aa5f52402e92cc842d6f948d506f2d73c
jeremiepiotte-districtm <jeremie.piotte@districtm.net>
2020-10-09 16:49:20 -0400

    Adding support for proto extensions

--- main.go
+++ main.go
@@ -294,6 +294,35 @@ func convertMessageType(curPkg *ProtoPackage, msg *descriptor.DescriptorProto, o
 			schema = append(schema, field)
 		}
 	}
+
+	extField := &Field{
+		Name:   "ext",
+		Type:   "RECORD",
+		Mode:   "NULLABLE",
+		Fields: []*Field{},
+	}
+	for _, ext := range msg.GetExtension() {
+		convertedExt, err := convertField(curPkg, ext, opts)
+		if err != nil {
+			glog.Errorf("Failed to convert field %s in %s: %v", ext.GetName(), msg.GetName(), err)
+			return nil, err
+		}
+
+		// Do we have a nested ext ?
+		for _, field := range convertedExt.Fields {
+			extField.Fields = append(extField.Fields, field)
+		}
+
+		// In case we didn't find nested extensions, add the actual ext
+		if len(extField.Fields) == 0 {
+			extField.Fields = append(extField.Fields, convertedExt)
+		}
+	}
+
+	if len(extField.Fields) > 0 {
+		schema = append(schema, extField)
+	}
+
 	return
 }
 
@@ -392,9 +421,61 @@ func convert(req *plugin.CodeGeneratorRequest) (*plugin.CodeGeneratorResponse, e
 		generateTargets[file] = true
 	}
 
+	extensions := []*descriptor.FieldDescriptorProto{}
+	for _, file := range req.GetProtoFile() {
+		for _, ext := range file.GetExtension() {
+			extensions = append(extensions, ext)
+		}
+	}
+
 	res := &plugin.CodeGeneratorResponse{}
 	for _, file := range req.GetProtoFile() {
 		for _, msg := range file.GetMessageType() {
+
+			for _, ext := range extensions {
+				extendeeNodes := strings.Split(ext.GetExtendee(), ".")
+				pkgNodes := strings.Split(file.GetPackage(), ".")
+
+				// We already know this extendee cannot be in this package.
+				if len(pkgNodes) > len(extendeeNodes) {
+					continue
+				}
+
+				// skipping leading "."
+				nodeIdx := 1
+				for _, node := range pkgNodes {
+					if node != extendeeNodes[nodeIdx] {
+						nodeIdx = 0
+						break
+					}
+
+					nodeIdx += 1
+				}
+
+				// Not the right package or message
+				if nodeIdx == 0 || msg.GetName() != extendeeNodes[nodeIdx] {
+					continue
+				}
+
+				foundNode := (*descriptor.DescriptorProto)(nil)
+				nestedMsgs := msg.GetNestedType()
+				for i := nodeIdx; i < len(extendeeNodes); i += 1 {
+					foundNode = nil
+					for _, nestedMsg := range nestedMsgs {
+						if nestedMsg.GetName() == extendeeNodes[i] {
+							foundNode = nestedMsg
+							break
+						}
+					}
+				}
+
+				if foundNode != nil {
+					foundNode.Extension = append(foundNode.Extension, ext)
+				} else {
+					msg.Extension = append(msg.Extension, ext)
+				}
+			}
+
 			glog.V(1).Infof("Loading a message type %s from package %s", msg.GetName(), file.GetPackage())
 			registerType(file.Package, msg)
 		}
@@ -416,6 +497,12 @@ func convert(req *plugin.CodeGeneratorRequest) (*plugin.CodeGeneratorResponse, e
 func convertFrom(rd io.Reader) (*plugin.CodeGeneratorResponse, error) {
 	glog.V(1).Info("Reading code generation request")
 	input, err := ioutil.ReadAll(rd)
+	//f, err := os.Create("/Users/jeremiepiotte/sandbox/proto_gen_input_test.txt")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//_, _ = f.Write(input)
+	//f.Close()
 	if err != nil {
 		glog.Error("Failed to read request:", err)
 		return nil, err
@@ -443,7 +530,7 @@ func commandLineParameters(parameter string) {
 		}
 	}
 
-	 for k, v := range param {
+	for k, v := range param {
 		switch k {
 		case "enumsasints":
 			if v == "true" {
